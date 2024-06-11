97b514f228a8bdc2c399dcf128fb231f5896d337
Gabriel Fu <hfu.gabriel@gmail.com>
2024-06-11 17:38:40 +0800

    update type

--- tui/navigation.go
+++ tui/navigation.go
@@ -13,12 +13,7 @@ func (m *NagivationModel) SetContent(content string) {
 	m.content = content
 }
 
-func (m NagivationModel) Init() tea.Cmd {
-	return nil
-}
-
-func (m NagivationModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
+func (m NagivationModel) Update(msg tea.Msg) (NagivationModel, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
