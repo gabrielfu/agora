3f5ecef543cc90c590c10569ae9960737ea7f322
Gabriel Fu <hfu.gabriel@gmail.com>
2024-06-14 14:42:30 +0800

    add tab bar

--- tui/panes/request_pane.go
+++ tui/panes/request_pane.go
@@ -1,6 +1,8 @@
 package panes
 
 import (
+	"strings"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/gabrielfu/tipi/tui/messages"
@@ -9,16 +11,28 @@ import (
 	"github.com/gabrielfu/tipi/tui/views"
 )
 
+var focusedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(styles.FocusBorderColor))
+
+type requestPaneTab int
+
+const (
+	paramsTab requestPaneTab = iota
+	headersTab
+	bodyTab
+)
+
 type RequestPaneModel struct {
 	width       int
 	height      int
 	borderColor string
 
 	rctx *states.RequestContext
+
+	tab requestPaneTab
 }
 
 func NewRequestPaneModel(rctx *states.RequestContext) RequestPaneModel {
-	return RequestPaneModel{rctx: rctx}
+	return RequestPaneModel{rctx: rctx, tab: paramsTab}
 }
 
 func (m *RequestPaneModel) SetWidth(width int) {
@@ -33,12 +47,25 @@ func (m *RequestPaneModel) SetBorderColor(color string) {
 	m.borderColor = color
 }
 
+func (m RequestPaneModel) renderTabBar() string {
+	tabs := []string{"Params", "Headers", "Body"}
+	tabs[m.tab] = focusedStyle.Render(tabs[m.tab])
+	return strings.Join(tabs, " - ") + "\n" + strings.Repeat(lipgloss.RoundedBorder().Bottom, m.width) + "\n"
+}
+
+func (m *RequestPaneModel) switchTab(direction int) {
+	m.tab = requestPaneTab((int(m.tab) + direction + 3) % 3)
+}
+
 func (m RequestPaneModel) generateStyle() lipgloss.Style {
 	border := styles.GenerateBorder(
 		lipgloss.RoundedBorder(),
 		styles.GenerateBorderOption{Title: []string{"[3]", "Request"}},
 		m.width,
 	)
+	// make the corner for the tab bar
+	border.Left = border.Left + border.MiddleLeft + strings.Repeat(border.Left, m.height)
+	border.Right = border.Right + border.MiddleRight + strings.Repeat(border.Right, m.height)
 	return lipgloss.NewStyle().
 		BorderStyle(border).
 		BorderForeground(lipgloss.Color(m.borderColor)).
@@ -52,6 +79,10 @@ func (m RequestPaneModel) Update(msg tea.Msg) (RequestPaneModel, tea.Cmd) {
 		switch msg.String() {
 		case "esc":
 			return m, messages.SetFocusCmd(views.CollectionPaneView)
+		case "[", "shift+tab":
+			m.switchTab(-1)
+		case "]", "tab":
+			m.switchTab(1)
 		}
 	}
 	return m, nil
@@ -59,11 +90,10 @@ func (m RequestPaneModel) Update(msg tea.Msg) (RequestPaneModel, tea.Cmd) {
 
 func (m RequestPaneModel) View() string {
 	var text string
+	text = m.renderTabBar()
 	if !m.rctx.Empty() {
 		request := m.rctx.Request()
-		if request != nil {
-			text = request.String()
-		}
+		text += request.String()
 	}
 	return m.generateStyle().Render(text)
 }
