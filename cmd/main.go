3dee49f12e90070318b08008ad58949b2e5de1ad
Gabriel Fu <hfu.gabriel@gmail.com>
2024-06-18 20:25:02 +0800

    put db in home dir

--- cmd/main.go
+++ cmd/main.go
@@ -3,25 +3,39 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gabrielfu/tipi/internal"
 	"github.com/gabrielfu/tipi/tui"
 )
 
-func main() {
-	db, err := internal.NewRquestDatabase("tipi.db")
+func Run() error {
+	home, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Printf("Error connecting to database: %v\n", err)
-		os.Exit(1)
+		return err
+	}
+	dir := filepath.Join(home, ".tipi")
+	if err := os.Mkdir(dir, 0755); err != nil && !os.IsExist(err) {
+		return err
+	}
+
+	dbFile := filepath.Join(dir, "data.sqlite")
+	db, err := internal.NewRquestDatabase(dbFile)
+	if err != nil {
+		return fmt.Errorf("error connecting to database: %v", err)
 	}
 	defer db.Close()
 
 	model := tui.NewRootModel(db, tui.WithCollectionPaneWidth(0.33))
 	program := tea.NewProgram(model, tea.WithAltScreen(), tea.WithMouseCellMotion())
+	_, err = program.Run()
+	return err
+}
 
-	if _, err := program.Run(); err != nil {
-		fmt.Printf("Uh oh, there was an error: %v\n", err)
+func main() {
+	if err := Run(); err != nil {
+		fmt.Printf("%v\n", err)
 		os.Exit(1)
 	}
 }
