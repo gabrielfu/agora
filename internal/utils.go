207e760d41a544e10698b7e55260ee0dbcd77334
Gabriel Fu <hfu.gabriel@gmail.com>
2024-10-24 12:56:46 +0800

    update random id

--- internal/utils.go
+++ internal/utils.go
@@ -4,6 +4,8 @@ import (
 	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
+const alphabets = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
 func RandomID() string {
-	return gonanoid.Must(16)
+	return gonanoid.MustGenerate(alphabets, 24)
 }
